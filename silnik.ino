97b01f2f766b86d61be5c3c188a2e70ed5ded689
Syslak7 <73444896+Syslak7@users.noreply.github.com>
2020-11-08 23:17:08 +0100

    Create silnik.ino
    do sterowania silniczkiem

---
+++ silnik.ino
@@ -0,0 +1,69 @@
+int kierunek1 = 5;
+int kierunek2 = 6;
+int btnprawo = 9;
+int btnlewo = 10;
+int predkosc = 7;
+int start = 11;
+
+
+bool obecny = LOW;
+bool poprzedni = LOW;
+bool ponownyOdczyt = LOW;
+bool wlaczony = LOW; 
+
+int ilestartow = 0; 
+
+void setup()
+{
+  pinMode(kierunek1, OUTPUT);
+  pinMode(kierunek2, OUTPUT);
+  pinMode(btnprawo, INPUT);
+  pinMode(btnlewo, INPUT);
+  pinMode(predkosc, OUTPUT);
+  pinMode(start, INPUT); 
+  
+  digitalWrite(kierunek1, LOW);
+  digitalWrite(kierunek2, LOW);
+  
+  Serial.begin(9600);
+}
+void loop()
+{
+  digitalWrite(predkosc, HIGH);
+  obecny = digitalRead(start);
+  if(obecny != poprzedni)
+  {
+    delay(10);
+    ponownyOdczyt = digitalRead(start);
+    if(ponownyOdczyt == obecny && obecny == HIGH)
+    {
+      wlaczony = !wlaczony; 
+      if(wlaczony == HIGH)
+      {
+        ilestartow++;
+        Serial.print("Liczba startow programu: ");
+        Serial.print(ilestartow);
+        Serial.print("\n");
+      }
+    }
+    poprzedni= obecny;
+  }
+  if(wlaczony == HIGH)
+  {
+    if(digitalRead(btnlewo) == HIGH)
+    {
+      digitalWrite(kierunek1, HIGH);
+      digitalWrite(kierunek2, LOW);
+    }
+    else if(digitalRead(btnprawo) == HIGH)
+    {
+      digitalWrite(kierunek1,LOW);
+      digitalWrite(kierunek2,HIGH);
+    }
+    else
+    {
+      digitalWrite(kierunek1,LOW);
+      digitalWrite(kierunek2,LOW);
+    }
+  }
+}
